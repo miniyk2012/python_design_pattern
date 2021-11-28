6002c415a681178ce6b3b13a31ee56688364d35f
yangkai.04 <yangkai.04@bytedance.com>
2021-11-28 19:40:41 +0800

    [feat] rm sth

--- source/conf.py
+++ source/conf.py
@@ -30,9 +30,8 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'recommonmark'
-]
+# simply add the extension to your list of extensions
+extensions = ['myst_parser']
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -48,7 +47,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh'
+language = 'zh_CN'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
